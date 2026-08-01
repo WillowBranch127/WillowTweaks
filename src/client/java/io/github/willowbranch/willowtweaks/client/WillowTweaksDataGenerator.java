7dd43c506ff6499d2ef77a4f94bdfa8f4aa590b0
WillowBranch127 <mvkzv4trjn@privaterelay.appleid.com>
2026-08-01 21:46:45 +0800

    first commit

---
+++ src/client/java/io/github/willowbranch/willowtweaks/client/WillowTweaksDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.willowbranch.willowtweaks.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WillowTweaksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
